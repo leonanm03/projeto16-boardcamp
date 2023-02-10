6da66a6d300daaef696d8f766dc91efa617517b1
Leonan <leonanm04@gmail.com>
2023-02-10 16:16:13 -0300

    fix: removed unused function (get rental by id)

--- src/controllers/rentals.js
+++ src/controllers/rentals.js
@@ -9,18 +9,3 @@ export async function getRentals(req, res) {
     res.status(500).send(error.message);
   }
 }
-
-// GET ONE
-export async function getRental(req, res) {
-  const { id } = req.params;
-  try {
-    const rental = await db.query(
-      `SELECT * FROM ${rentalsTable} WHERE id = $1;`,
-      [id]
-    );
-    res.status(200).send(rental.rows[0]);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-}
-
